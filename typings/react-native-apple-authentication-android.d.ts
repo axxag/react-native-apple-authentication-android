2b04be259fdc6471f2fc6d77b76097db434e35ec
Alex Gentile <agentilela@gmail.com>
2020-07-06 13:03:07 -0700

    finish up typings

--- typings/react-native-apple-authentication-android.d.ts
+++ typings/react-native-apple-authentication-android.d.ts
@@ -1,4 +1,8 @@
 declare module "react-native-apple-authentication-android" {
+  export const NOT_CONFIGURED_ERROR: string;
+  export const SIGNIN_FAILED_ERROR: string;
+  export const SIGNIN_CANCELLED_ERROR: string;
+
   interface IRNAppleAuthAndroidConfig {
     clientId: string;
     redirectUri: string;
