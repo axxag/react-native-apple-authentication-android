7a6f25bc22f4392b5fe5dd0d3673c22f528e9373
Alex Gentile <agentilela@gmail.com>
2020-07-08 15:20:21 -0700

    prevent "null is not an object" errors on ios

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ import { NativeModules } from "react-native";
 const { AppleAuthenticationAndroid } = NativeModules;
 
 export const NOT_CONFIGURED_ERROR =
+  AppleAuthenticationAndroid &&
   AppleAuthenticationAndroid.E_NOT_CONFIGURED_ERROR;
 export const SIGNIN_FAILED_ERROR =
+  AppleAuthenticationAndroid &&
   AppleAuthenticationAndroid.E_SIGNIN_FAILED_ERROR;
 export const SIGNIN_CANCELLED_ERROR =
+  AppleAuthenticationAndroid &&
   AppleAuthenticationAndroid.E_SIGNIN_CANCELLED_ERROR;
 
-export const Scope = AppleAuthenticationAndroid.Scope;
-export const ResponseType = AppleAuthenticationAndroid.ResponseType;
+export const Scope =
+  AppleAuthenticationAndroid && AppleAuthenticationAndroid.Scope;
+export const ResponseType =
+  AppleAuthenticationAndroid && AppleAuthenticationAndroid.ResponseType;
 
 export default AppleAuthenticationAndroid;
